49b09928891af75c46a8b6e4d728681e37655e9a
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-05-30 13:47:42 +0400

    Добавьте файлы проекта.

---
+++ ЬфесрПфьу/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animals = new List<string>()
+            {
+                "🐛", "🐛",
+                "🕷️", "🕷️",
+                "🐜", "🐜",
+                "🐝", "🐝",
+                "🐞", "🐞",
+                "🦟", "🦟",
+                "🦗", "🦗",
+                "🦋", "🦋",
+            };
+
+            Random Random = new Random();
+            foreach (TextBlock textBlock in MainGrid.Children.OfType<TextBlock>())
+            {
+                int index = Random.Next(animals.Count);
+                string nextAnimal = animals[index];
+                textBlock.Text = nextAnimal;
+                animals.RemoveAt(index);
+            } 
+        }
+    }
+}
