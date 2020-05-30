18a6b9e326120ea92072584de052b07f601f2379
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-05-30 16:50:16 +0400

    Added timer

--- ЬфесрПфьу/MainWindow.xaml.cs
+++ ЬфесрПфьу/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MatchGame
 {
@@ -20,10 +21,35 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer Timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
+            Timer.Interval = TimeSpan.FromSeconds(.1);
+            Timer.Tick += Timer_Tick;
+            
             InitializeComponent();
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBox.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                Timer.Stop();
+                timeTextBox.Text = timeTextBox.Text + " - Play again?";
+            }
+        }
+
+        private void TimeTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
         }
 
         private void SetUpGame()
@@ -43,11 +69,357 @@ namespace MatchGame
             Random Random = new Random();
             foreach (TextBlock textBlock in MainGrid.Children.OfType<TextBlock>())
             {
-                int index = Random.Next(animals.Count);
+                if (textBlock.Name != "timeTextBox") { 
+                 int index = Random.Next(animals.Count);
                 string nextAnimal = animals[index];
                 textBlock.Text = nextAnimal;
                 animals.RemoveAt(index);
-            } 
+            }
+            }
+            Timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        TextBlock LastTextBlockClicked;
+        bool FindingMatch = false; 
+
+        private void textBlock0_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock; 
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock01_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock02_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock03_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false; 
+            }
+        }
+
+        private void textBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false; 
+            }
+        }
+
+        private void textBlock5_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false; 
+            }
+        }
+
+        private void textBlock_Copy1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false; 
+            }
+        }
+
+        private void textBlock2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock2222_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock33_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock_Copy12_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock_Copy111_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock222_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock24_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock52_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
+        }
+
+        private void textBlock53_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (FindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                LastTextBlockClicked = textBlock;
+                FindingMatch = true;
+            }
+            else if (textBlock.Text == LastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                FindingMatch = false; matchesFound++;
+            }
+            else
+            {
+                LastTextBlockClicked.Visibility = Visibility.Visible;
+                FindingMatch = false;
+            }
         }
     }
+
+
 }
